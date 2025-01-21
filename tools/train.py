ed74d20a99dd0ac799bebb5051c54c1b16816d41
Terje Farup <terjefarup98@gmail.com>
2025-01-21 10:04:01 +0100

    train maptrack split

--- tools/train.py
+++ tools/train.py
@@ -140,18 +140,31 @@ def main():
                 plg_lib = importlib.import_module(_module_path)
 
             from projects.mmdet3d_plugin.bevformer.apis.train import custom_train_model
-    # set cudnn_benchmark
-    if cfg.get('cudnn_benchmark', False):
-        torch.backends.cudnn.benchmark = True
+    
 
+
+    config_name = cfg.filename.split("/")[-1].split(".")[0]
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    # work_dir is determined in this priority: CLI > segment in file > filename
     # work_dir is determined in this priority: CLI > segment in file > filename
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
-        cfg.work_dir = args.work_dir
+        
+        if not os.path.exists(path_log_temp  := osp.join(args.work_dir, f'{config_name}_{timestamp}')):
+            os.makedirs(path_log_temp)
+
+        cfg.work_dir = path_log_temp
+
     elif cfg.get('work_dir', None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
+        
+
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+
     # if args.resume_from is not None:
     if args.resume_from is not None and osp.isfile(args.resume_from):
         cfg.resume_from = args.resume_from
@@ -180,7 +193,7 @@ def main():
     # dump config
     cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config)))
     # init the logger before other steps
-    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    #timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
     log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
     # specify logger name, if we still use 'mmdet', the output info will be
     # filtered and won't be saved in the log_file
